0a42f9eb6337ded3c3c1c953cfc4db09ccefec7f
Adarsh Singh <adarsh.singh@hashedin.com>
2021-04-16 13:55:39 +0530

    modified user entity

--- src/main/java/com/example/cars/entities/user.java
+++ src/main/java/com/example/cars/entities/user.java
@@ -1,4 +1,86 @@
 package com.example.cars.entities;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class user {
+
+    public user(Integer userId, String userName, String userEmail, String userPassword, String address, String phoneNumber) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Integer userId;
+
+    @Column
+    private String userName;
+
+    @Column
+    private String userEmail;
+
+    @Column
+    private String userPassword;
+
+    @Column
+    private String address;
+
+    @Column
+    private String phoneNumber;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
