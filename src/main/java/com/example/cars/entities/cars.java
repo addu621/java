687ffe27747533426fdbb4887d1a94de319623c3
Adarsh Singh <adarsh.singh@hashedin.com>
2021-04-16 14:38:43 +0530

    modified cars, carsBrand and modelDetails entities

--- src/main/java/com/example/cars/entities/cars.java
+++ src/main/java/com/example/cars/entities/cars.java
@@ -1,4 +1,49 @@
 package com.example.cars.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
 public class cars {
+
+    public cars(Integer carId, Integer brandId, String carName) {
+        this.carId = carId;
+        this.brandId = brandId;
+        this.carName = carName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Integer carId;
+
+    @Column
+    private Integer brandId;
+
+    @Column
+    private String carName;
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
 }
--- src/main/java/com/example/cars/entities/modelDetails.java
+++ src/main/java/com/example/cars/entities/modelDetails.java
@@ -1,4 +1,54 @@
 package com.example.cars.entities;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "modelDetails")
 public class modelDetails {
+
+    public modelDetails(Integer modelId, Integer carId, String modelName) {
+        this.modelId = modelId;
+        this.carId = carId;
+        this.modelName = modelName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Integer modelId;
+
+    @Column
+    private Integer carId;
+
+    @Column
+    private String modelName;
+
+    /*
+        more details to be included
+    */
+
+    public Integer getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(Integer modelId) {
+        this.modelId = modelId;
+    }
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
 }
