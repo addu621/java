0f5555d9d564b34959b3d164e0291dab6d5d7737
Adarsh Singh <adarsh.singh@hashedin.com>
2021-04-17 20:19:51 +0530

    added MySQL configurations

--- src/main/java/com/example/cars/entities/buyers.java
+++ src/main/java/com/example/cars/entities/buyers.java
@@ -1,38 +1,38 @@
-package com.example.cars.entities;
-
-import javax.persistence.*;
-import java.lang.reflect.Array;
-
-@Entity
-@Table(name = "buyers")
-public class buyers {
-
-    public buyers(Integer userId, Array purchase) {
-        this.userId = userId;
-        this.purchase = purchase;
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column
-    private Integer userId;
-
-    @Column
-    private Array purchase;
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public Array getPurchase() {
-        return purchase;
-    }
-
-    public void setPurchase(Array purchase) {
-        this.purchase = purchase;
-    }
-}
+//package com.example.cars.entities;
+//
+//import javax.persistence.*;
+//import java.lang.reflect.Array;
+//
+//@Entity
+//@Table(name = "buyers")
+//public class buyers {
+//
+//    public buyers(Integer userId, Array purchase) {
+//        this.userId = userId;
+//        this.purchase = purchase;
+//    }
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column
+//    private Integer userId;
+//
+//    @Column
+//    private Array purchase;
+//
+//    public Integer getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(Integer userId) {
+//        this.userId = userId;
+//    }
+//
+//    public Array getPurchase() {
+//        return purchase;
+//    }
+//
+//    public void setPurchase(Array purchase) {
+//        this.purchase = purchase;
+//    }
+//}
--- src/main/java/com/example/cars/entities/sellers.java
+++ src/main/java/com/example/cars/entities/sellers.java
@@ -1,50 +1,50 @@
-package com.example.cars.entities;
-
-import javax.persistence.*;
-import java.lang.reflect.Array;
-
-@Entity
-@Table(name = "sellers")
-public class sellers {
-
-    public sellers(Integer userId, Array posts, Array sold) {
-        this.userId = userId;
-        this.posts = posts;
-        this.sold = sold;
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column
-    private Integer userId;
-
-    @Column
-    private Array posts;
-
-    @Column
-    private Array sold;
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public Array getPosts() {
-        return posts;
-    }
-
-    public void setPosts(Array posts) {
-        this.posts = posts;
-    }
-
-    public Array getSold() {
-        return sold;
-    }
-
-    public void setSold(Array sold) {
-        this.sold = sold;
-    }
-}
+//package com.example.cars.entities;
+//
+//import javax.persistence.*;
+//import java.lang.reflect.Array;
+//
+//@Entity
+//@Table(name = "sellers")
+//public class sellers {
+//
+//    public sellers(Integer userId, Array posts, Array sold) {
+//        this.userId = userId;
+//        this.posts = posts;
+//        this.sold = sold;
+//    }
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column
+//    private Integer userId;
+//
+//    @Column
+//    private Array posts;
+//
+//    @Column
+//    private Array sold;
+//
+//    public Integer getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(Integer userId) {
+//        this.userId = userId;
+//    }
+//
+//    public Array getPosts() {
+//        return posts;
+//    }
+//
+//    public void setPosts(Array posts) {
+//        this.posts = posts;
+//    }
+//
+//    public Array getSold() {
+//        return sold;
+//    }
+//
+//    public void setSold(Array sold) {
+//        this.sold = sold;
+//    }
+//}
