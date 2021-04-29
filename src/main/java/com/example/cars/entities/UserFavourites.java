dde62f2f46d93f0669d46f5e76f8d728c1d39ca7
Adarsh Singh <adarsh.singh@hashedin.com>
2021-04-29 13:49:46 +0530

    Added UserFavourites Table

---
+++ src/main/java/com/example/cars/entities/UserFavourites.java
@@ -0,0 +1,49 @@
+package com.example.cars.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_favourites")
+public class UserFavourites {
+
+    public UserFavourites(Integer id, Integer userId, Integer carId) {
+        this.id = id;
+        this.userId = userId;
+        this.carId = carId;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+
+    @Column
+    private Integer userId;
+
+    @Column
+    private Integer carId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+}
